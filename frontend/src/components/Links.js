cbf757aebaa841f57659eb50538c1226d5de0758
Gustavo Arellano <gustavo813arellano@gmail.com>
2020-04-10 20:17:49 -0700

    Frontend updates

--- frontend/src/components/Links.js
+++ frontend/src/components/Links.js
@@ -1,19 +1,54 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 import NavBar from './NavBar';
 
+const UserFirstLastName = props => (
+    <div className="user-row">
+        <p className="user-data">
+            <a href="#">{ props.user.firstName  + " " + props.user.lastName } </a>
+        </p>
+
+    </div>
+)
 
 class Links extends Component {
-    constructor() {
-        super()
-        this.state = {}
+    constructor(props) {
+        super(props)
+        this.state = {
+            users: []
+        }
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:8000/user/users')
+        .then( response => {
+            this.setState({ users: response.data})
+        })
+        .catch( (error) => {
+            console.log(error)
+        })
+    }
+
+    usersFirstLastNameList() {
+        return this.state.users.map(user => { 
+            return <UserFirstLastName user={ user } key={ user._id } />
+        })
     }
 
     render() {
         return(
 
             <div>
+
                 <NavBar />
-                I will display links!
+                
+                <h1>Display By Links</h1>
+
+                <h4>Click on a user to show data!</h4>
+
+                <p>{ this.usersFirstLastNameList() }</p>
+
             </div>
 
             )
